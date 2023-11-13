079725fdb5c898db2626786d50139200dcb02748
elfisch <larsfisch@gmail.com>
2023-11-13 18:33:32 -0500

    added oddish layout for keychron q3

--- compile_keychron_q3.sh
+++ compile_keychron_q3.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 qmk compile -kb keychron/q3/ansi -km cubone && mkdir -p output && mv keychron_q3_ansi_cubone.bin output
+qmk compile -kb keychron/q3/ansi -km oddish && mkdir -p output && mv keychron_q3_ansi_oddish.bin output

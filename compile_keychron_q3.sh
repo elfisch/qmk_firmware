fda43869192a17ec7cfaf8911f49ee248e13ba38
elfisch <larsfisch@gmail.com>
2023-11-13 18:51:33 -0500

    changed name of keychron bin output

--- compile_keychron_q3.sh
+++ compile_keychron_q3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-qmk compile -kb keychron/q3/ansi -km cubone && mkdir -p output && mv keychron_q3_ansi_cubone.bin output
-qmk compile -kb keychron/q3/ansi -km oddish && mkdir -p output && mv keychron_q3_ansi_oddish.bin output
+qmk compile -kb keychron/q3/ansi -km cubone && mkdir -p output && mv keychron_q3_ansi_cubone.bin output/keychron_q3_cubone.bin 
+qmk compile -kb keychron/q3/ansi -km oddish && mkdir -p output && mv keychron_q3_ansi_oddish.bin output/keychron_q3_oddish.bin 

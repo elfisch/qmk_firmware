28e1f0ab3dff97275edf03267c4c482dcfd306f8
elfisch <larsfisch@gmail.com>
2023-11-16 11:59:15 -0500

    cubone layout for cannonkeys db60 (for bakeneko60)

---
+++ compile_cannonkeys_db60.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+qmk compile -kb cannonkeys/db60/hotswap -km cubone && mkdir -p output && mv cannonkeys_db60_hotswap_cubone.bin output/
+printf "\n\n********************************************************************************\nNOTE: to properly update keymap on fresh PCB, you may have to compile and flash with VIA disabled and then use the EE_CLR key. Then compiling with VIA enabled and flash. After that you can leave VIA enabled (I think), but use EE_CLR after each flash (unless you change the EE_CLR key, then start over with VIA disabled).\n********************************************************************************\n"

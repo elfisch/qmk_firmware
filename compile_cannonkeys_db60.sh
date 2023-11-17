1b03d459db9615f67496017bae174b88f1636d7b
elfisch <larsfisch@gmail.com>
2023-11-17 03:42:47 -0500

    cubone and oddish layout for db60 solder pch

--- compile_cannonkeys_db60.sh
+++ compile_cannonkeys_db60.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+qmk compile -kb cannonkeys/db60/rev2 -km cubone && mkdir -p output && mv cannonkeys_db60_rev2_cubone.bin output/cannonkeys_db60_solder_cubone.bin
+qmk compile -kb cannonkeys/db60/rev2 -km oddish && mkdir -p output && mv cannonkeys_db60_rev2_oddish.bin output/cannonkeys_db60_solder_oddish.bin
 qmk compile -kb cannonkeys/db60/hotswap -km cubone && mkdir -p output && mv cannonkeys_db60_hotswap_cubone.bin output/
 qmk compile -kb cannonkeys/db60/hotswap -km oddish && mkdir -p output && mv cannonkeys_db60_hotswap_oddish.bin output/
 printf "\n\n********************************************************************************\nNOTE: to properly update keymap on fresh PCB, you may have to compile and flash with VIA disabled and then use the EE_CLR key. Then compiling with VIA enabled and flash. After that you can leave VIA enabled (I think), but use EE_CLR after each flash (unless you change the EE_CLR key, then start over with VIA disabled).\n********************************************************************************\n"

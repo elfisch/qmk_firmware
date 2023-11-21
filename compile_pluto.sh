cc4efae9a69476b0e00fadc3106c8eb622c39e67
elfisch <larsfisch@gmail.com>
2023-11-21 07:01:47 -0500

    cubone layout for pluto

--- compile_pluto.sh
+++ compile_pluto.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+qmk compile -kb dztech/pluto -km cubone && mkdir -p output && mv dztech_pluto_cubone.uf2 output/
 qmk compile -kb dztech/pluto -km oddish && mkdir -p output && mv dztech_pluto_oddish.uf2 output/

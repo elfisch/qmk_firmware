1d8a660dfd2a5d2e6f9c33fea3642c36c8e5399c
elfisch <larsfisch@gmail.com>
2023-11-21 04:28:15 -0500

    oddish layout for keychron v4

--- compile_keychron.sh
+++ compile_keychron.sh
@@ -2,3 +2,4 @@
 qmk compile -kb keychron/q3/ansi -km cubone && mkdir -p output && mv keychron_q3_ansi_cubone.bin output/keychron_q3_cubone.bin
 qmk compile -kb keychron/q3/ansi -km oddish && mkdir -p output && mv keychron_q3_ansi_oddish.bin output/keychron_q3_oddish.bin
 qmk compile -kb keychron/v4/ansi -km cubone && mkdir -p output && mv keychron_v4_ansi_cubone.bin output/keychron_v4_cubone.bin
+qmk compile -kb keychron/v4/ansi -km oddish && mkdir -p output && mv keychron_v4_ansi_oddish.bin output/keychron_v4_oddish.bin

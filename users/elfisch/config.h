3ccefe59a30ce8988749dffa68c64c7e7810028a
elfisch <larsfisch@gmail.com>
2024-05-15 19:34:31 -0400

    enable QK_MAKE

--- users/elfisch/config.h
+++ users/elfisch/config.h
@@ -1,4 +1,9 @@
 #pragma once
 
 #define TAPPING_TERM 150
+
+// Sends qmk compile -kb (keyboard) -km (keymap), or qmk flash if shift is held.
+// Puts keyboard into bootloader mode if shift & control are held.
+#define ENABLE_COMPILE_KEYCODE
+
 // #define FORCE_NKRO

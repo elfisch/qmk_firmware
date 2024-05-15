265d386e225a2c825638053b9ecf6e0bf3b9e45d
elfisch <larsfisch@gmail.com>
2024-05-15 19:43:12 -0400

    og60 was renamed to tofu60

--- compile_tofu60_20.sh
+++ compile_tofu60_20.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-qmk compile -kb dztech/og60 -km cubone && mkdir -p output && mv dztech_og60_cubone.uf2 output/
-qmk compile -kb dztech/og60 -km oddish && mkdir -p output && mv dztech_og60_oddish.uf2 output/
+qmk compile -kb dztech/tofu60 -km cubone && mkdir -p output && mv dztech_tofu60_cubone.uf2 output/
+qmk compile -kb dztech/tofu60 -km oddish && mkdir -p output && mv dztech_tofu60_oddish.uf2 output/

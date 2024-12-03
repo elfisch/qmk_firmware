3448fa967421c940044d93f82cc12f4da8ad603f
elfisch <larsfisch@gmail.com>
2024-12-02 20:44:35 -0500

    updates to keychron v4 cubone layout

--- keyboards/keychron/v4/ansi/keymaps/cubone/keymap.c
+++ keyboards/keychron/v4/ansi/keymaps/cubone/keymap.c
@@ -31,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,               KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,                  KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,
         MT(MOD_LCTL, KC_ESC), KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,                  KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_LALT,             MT(MOD_RCTL, KC_ENT),
         SC_LSPO,              KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                  KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,             SC_RSPC,
-        KC_LCTL,  KC_LGUI,    KC_LALT,                             LT(WIN_BASE, KC_SPC),                              KC_RALT,  KC_RGUI,  MO(_FN1),  MO(_FN2)),
+        KC_LCTL,  KC_LGUI,    KC_LALT,                             LT(WIN_BASE, KC_SPC),                              KC_RALT,  MO(_FN1),  MO(_FN2),  KC_RCTL),
 
     [WIN_BASE] = LAYOUT_60_ansi(
         KC_GRV,   KC_F1,       KC_F2,    KC_F3,       KC_F4,       KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,
         ALT_TAB,  S(KC_LBRC),  KC_LBRC,  KC_RBRC,     S(KC_RBRC),  KC_PMNS,  KC_PPLS,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,
-        _______,  KC_LALT,     KC_SPC,   KC_ENT,      KC_MPLY,     KC_DEL,   KC_BSPC,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,            _______,
+        _______,  KC_LALT,     KC_SPC,   _______,     KC_MPLY,     KC_DEL,   KC_BSPC,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,            KC_MPLY,
         _______,  S(KC_BSLS),  KC_QUOT,  S(KC_QUOT),  S(KC_MINS),  _______,  _______,  KC_EQL,   _______,  _______,  KC_BSLS,                      _______,
-        _______,  _______,     _______,                            _______,                                          _______,  _______,  _______,  _______),
+        _______,  _______,     _______,                            _______,                                          _______,  KC_RGUI,  _______,  _______),
 
     [_FN1] = LAYOUT_60_ansi(
         QK_GESC,  KC_P1,    KC_P2,    KC_P3,    KC_P4,    KC_P5,    KC_P6,    KC_P7,    KC_P8,    KC_P9,    KC_P0,    KC_PMNS,  KC_PPLS,  QK_BOOT,
